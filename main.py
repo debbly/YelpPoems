7528723f04c4ba0030060a7649ac655081455d3a
Debbie Ly <debbs.ly@gmail.com>
2016-04-19 20:32:34 -0400

    updated main in main.py to have routing

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Flask, render_template
+
+import ling
+
+app = Flask(__name__)
+all_words_dict = {}
+
+# Views
+@app.route('/')
+@app.route('/index', methods=['GET'])
+def index():
+    global all_words_dict
+    all_words_dict = {}
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
