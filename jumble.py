15a0aa3937b011bd455c1389a3c5bc89732dc75e
Debbie Ly <debbs.ly@gmail.com>
2016-04-21 12:22:25 -0400

    Added main to jumble

--- jumble.py
+++ jumble.py
@@ -1,3 +1,7 @@
-def jumble(text):
+def main():
+	lines = jumble()
+	return lines
+
+def jumble():
 	#parse by new line and then jumble up by randomly swapping lines
 	#look at a corpus of words that are positive or negative and have the words bolded or removed
