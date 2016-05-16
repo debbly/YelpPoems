b57c1ff558c6e38ed2c0f10ccf468f3010c4e679
Debbie Ly <debbs.ly@gmail.com>
2016-05-15 22:34:58 -0400

    Cleaned up file

--- jumble.py
+++ jumble.py
@@ -5,13 +5,12 @@ def main():
 	return lines
 
 def jumble():
-	#parse by new line and then jumble up by randomly swapping lines
 	#look at a corpus of words that are positive or negative and have the words bolded or removed
 	random1 = random.randint(1, len(all_words_dict))
 	random2 = random.randint(1, len(all_words_dict))
 
 	line1 = all_words_dict[random1]
 	all_words_dict[random1] = all_words_dict[random2]
-	all_words_dict[random2] = line1;
+	all_words_dict[random2] = line1
 
 	return all_words_dict
